af5afc2d1c02651eeb4e57bba54905d3a2ee8d42
Ritik Mitra <ritikmitra36@gmail.com>
2023-12-29 23:27:08 +0530

    sql generated

--- profile.php
+++ profile.php
@@ -17,80 +17,250 @@ while ($rows = mysqli_fetch_array($query)) {
   $dob = $rows['dob'];
   $mob = $rows['mobileNumber'];
 }
+
+//sql for employement details
+$sql = "SELECT * FROM `employmentdetails` WHERE `id`='$user_id'";
+$query = mysqli_query($conn, $sql);
+while ($rows = mysqli_fetch_array($query)) {
+  $Company_name = $rows['companyName'];
+  $designation = $rows['designation'];
+  $employee_id = $rows['employeeId'];
+  $job_tenure = $rows['jobTenure'];
+  $pincode = $rows['pincode'];
+  $city = $rows['city'];
+}
+//sql for kyc details
+$sql = "SELECT * FROM `kycdetails` WHERE `id`='$user_id'";
+$query = mysqli_query($conn, $sql);
+while ($rows = mysqli_fetch_array($query)) {
+  $aadhaar_number = $rows['aadharNo'];
+  $panCard_number = $rows['panCardNo'];
+  $electricity_bill = $rows['electricityBill'];
+  $driving_license = $rows['drivingLicense'];
+  $bankAccount_number = $rows['bankAccountNo'];
+  $ifsc_code = $rows['ifscCode'];
+  $salary_slip_current_month_path = $rows['salarySlipCurrentMonthPath'];
+}
+
 ?>
 
 <div class="row">
-<div class="col-lg-4 grid-margin stretch-card">
-  <div class="card mb-4">
-    <div class="card-body text-center">
-      <!-- live Selfie upload -->
-      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="Live Selfie" class="rounded-circle img-fluid" style="width: 150px;">
-      <h5 class="my-3">
-        <?php echo $user_name; ?>
-      </h5>
-      <!-- role of the employee -->
-      <p class="text-muted mb-1">Full Stack Developer</p>
-      <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
-      <div class="d-flex justify-content-center mb-2">
-        <!-- view Salary slip -->
-        <button type="button" class="btn btn-primary">Follow</button>
-        <button type="button" class="btn btn-outline-primary ms-1">Message</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="col-lg-8 grid-margin stretch-card">
-  <div class="card mb-4">
-    <div class="card-body">
-      <div class="row">
-        <div class="col-sm-3">
-          <p class="mb-0">Full Name</p>
-        </div>
-        <div class="col-sm-9">
-          <p class="text-muted mb-0"><?php echo $user_name ?></p>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-sm-3">
-          <p class="mb-0">Email</p>
-        </div>
-        <div class="col-sm-9">
-          <p class="text-muted mb-0"><?php echo $email ?></p>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-sm-3">
-          <p class="mb-0">Mobile</p>
-        </div>
-        <div class="col-sm-9">
-          <p class="text-muted mb-0"><?php echo $mob ?></p>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-sm-3">
-          <p class="mb-0">Gender</p>
-        </div>
-        <div class="col-sm-9">
-          <p class="text-muted mb-0"><?php echo $gender ?></p>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-sm-3">
-          <p class="mb-0">Address</p>
-        </div>
-        <div class="col-sm-9">
-          <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+  <div class="col-lg-4 grid-margin stretch-card">
+    <div class="card mb-4">
+      <div class="card-body text-center">
+        <!-- live Selfie upload -->
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="Live Selfie" class="rounded-circle img-fluid" style="width: 150px;">
+        <h5 class="my-3">
+          <?php echo $user_name; ?>
+        </h5>
+        <!-- role of the employee -->
+        <p class="text-muted mb-1">Full Stack Developer</p>
+        <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+        <div class="d-flex justify-content-center mb-2">
+          <!-- view Salary slip -->
+          <button type="button" class="btn btn-primary">Follow</button>
+          <button type="button" class="btn btn-outline-primary ms-1">Message</button>
         </div>
       </div>
     </div>
   </div>
+  <!-- Basic Details -->
+  <div class="col-lg-8 grid-margin stretch-card">
+    <div class="card mb-4">
+      <div class="card-body">
+        <div class="row">
+          <h3 class="mb-3">Basic Details</h3>
+          <div class="col-sm-3">
+            <p class="mb-0">Full Name</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $user_name ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Email</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $email ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Mobile</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $mob ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Gender</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $gender ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Address</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+<!-- Employee Details -->
+<div class="row">
+  <div class="col-lg-6 grid-margin stretch-card">
+    <div class="card mb-4">
+      <div class="card-body">
+        <div class="row">
+          <h3 class="mb-3">Employee Details</h3>
+          <div class="col-sm-3">
+            <p class="mb-0">Company Name</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $Company_name ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Designation</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $designation ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Job Tenure</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $job_tenure ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Employee ID</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $employee_id ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">Pincode</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $pincode ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">City</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo $city ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-3">
+            <p class="mb-0">ID Card Pic Live</p>
+          </div>
+          <div class="col-sm-9">
+            <p class="text-muted mb-0"><?php echo "dmemo" ?></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- kyc Details -->
+  <div class="col-lg-6 grid-margin stretch-card">
+    <div class="card mb-4">
+      <div class="card-body">
+        <div class="row">
+          <h3 class="mb-3">KYC Details</h3>
+          <div class="col-sm-6">
+            <p class="mb-0">Aadhaar Card Number </p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $aadhaar_number ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="mb-0">PanCard Number</p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $panCard_number ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="mb-0">Electricity Bill</p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $electricity_bill ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="mb-0">Driving License </p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $driving_license ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="mb-0">Bank Account Number</p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $bankAccount_number ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="mb-0">IFSC Code </p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $ifsc_code ?></p>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="mb-0">Salary Slip Current Month Path</p>
+          </div>
+          <div class="col-sm-6">
+            <p class="text-muted mb-0"><?php echo $salary_slip_current_month_path ?></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-  <!-- <div class="col-lg-12 grid-margin stretch-card">
+<!-- <div class="col-lg-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
                   <h4 class="card-title">Striped Table</h4>
